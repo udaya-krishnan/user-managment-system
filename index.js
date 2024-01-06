c609648d10a5d48f1f8da8acb31908c0da1e885d
udaya-krishnan <150499864+udaya-krishnan@users.noreply.github.com>
2024-01-06 15:38:42 +0530

    usermanagment system

---
+++ index.js
@@ -0,0 +1,36 @@
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser")
+mongoose.connect("mongodb://localhost:27017/user_management_system");
+const path=require("path")
+const express=require("express")
+const flash=require("express-flash")
+
+const nocache=require("nocache")
+ 
+const app=express();  
+app.use(nocache())
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+     
+app.use("/static", express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
+
+app.use(express.static('views'))
+ 
+
+app.use(flash());
+ 
+// for user routes
+const userRoute=require("./routers/userRoute")  
+app.use("/",userRoute) 
+
+// for admin routes 
+const adminRoute=require("./routers/adminRoute")
+app.use("/admin",adminRoute) 
+
+  
+
+app.listen(2003,()=>{
+    console.log("srever running")
+})  
